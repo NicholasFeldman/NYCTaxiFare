f9e03ce0526d2b6ed66fa444e99eedeb5fb3bc43
Bill Tarr <betarr13@gmail.com>
2018-11-30 15:13:46 -0500

    update file path

--- processCSVs.py
+++ processCSVs.py
@@ -7,11 +7,13 @@ PU_DATETIME = 1
 PASSENGER_COUNT = 3
 PU_LOCATION = 7
 DO_LOCATION = 8
-TOTAL = 16
+TOTAL = 10
 DATA_TO_KEEP = [PASSENGER_COUNT, PU_LOCATION, DO_LOCATION, TOTAL]
+CSV_HEADERS = ','.join(['passenger_count', 'PULocationID', 'DOLocationID', 'fare_amount', 'year', 'month', 'hour'])
 
-def write_headers():
-    pass
+def write_headers(file_path):
+    with open(file_path, 'w') as write_file:
+        write_file.write(CSV_HEADERS + '\n')
 
 def datetime_to_data(date):
     date_info = datetime.strptime(date[:16], '%Y-%m-%d %H:%M')
@@ -26,6 +28,10 @@ def process_data(line):
 
 def main():
     count = 0
+    train_path = os.path.join(os.getcwd() + '/output/', 'train.csv')
+    test_path = os.path.join(os.getcwd() + '/output/', 'test.csv')
+    write_headers(train_path)
+    write_headers(test_path)
     for filename in glob.glob(os.path.join(os.getcwd() + '/input', '*.csv')):
         print('reading: ' + filename)
         for line in open(filename, 'r'):
@@ -33,12 +39,12 @@ def main():
             if processed_data == None:
                 continue
             if count == 3:
-                with open(os.path.join(os.getcwd() + '/output/', 'test.csv'), 'a') as trainFile:
-                    trainFile.write(processed_data + '\n')
+                with open(test_path, 'a') as test_file:
+                    test_file.write(processed_data + '\n')
                 count = 0
             else:
                 count += 1
-                with open(os.path.join(os.getcwd() + '/output/', 'train.csv'), 'a') as trainFile:
-                    trainFile.write(processed_data + '\n')
+                with open(train_path, 'a') as train_file:
+                    train_file.write(processed_data + '\n')
 
 main()
