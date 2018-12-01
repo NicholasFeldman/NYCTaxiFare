55564be250d1cd1eff79eb0befbb940a0d20b474
Bill Tarr <betarr13@gmail.com>
2018-12-01 14:44:39 -0500

    update to match new data format

--- NYCTaxiFare.py
+++ NYCTaxiFare.py
@@ -8,23 +8,6 @@ training_data = pd.read_csv('train.csv')
 print("Reading Testing Data...")
 test_data = pd.read_csv('test.csv')
 
-
-def add_year_month_hour_to_data(data):
-    data['tpep_pickup_datetime'] = data['tpep_pickup_datetime'].str.slice(0, 16)
-    data['tpep_pickup_datetime'] = pd.to_datetime(data['tpep_pickup_datetime'], utc=True, format='%Y-%m-%d %H:%M')
-    data['year'] = pd.DatetimeIndex(data['tpep_pickup_datetime']).year
-    data['month'] = pd.DatetimeIndex(data['tpep_pickup_datetime']).month
-    data['hour'] = pd.DatetimeIndex(data['tpep_pickup_datetime']).hour
-    return data
-
-
-print("Adding Year, Month, and Hour to Training Data...")
-training_data = add_year_month_hour_to_data(training_data)
-
-print("Adding Year, Month, and Hour to Testing Data...")
-test_data = add_year_month_hour_to_data(test_data)
-
-
 def create_column_stack(df):
     return np.column_stack((df.year, df.month, df.hour, df.PULocationID, df.DOLocationID))
 
